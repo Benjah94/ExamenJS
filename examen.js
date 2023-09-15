db1081a05d7753b80ed53be26dfabccc385c8af3
Benjah94 <136342025+Benjah94@users.noreply.github.com>
2023-09-15 08:47:17 -0300

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,51 @@
+function salarioAnual() {
+let salarioAnual = 21600
+return (salarioAnual / 12)
+}
+console.log(salarioAnual())
+function salarioMensual(){
+let salarioMensual = 1500
+return (salarioMensual * 12)
+}
+console.log(salarioMensual())
+calcularPromedio()
+{
+let nota1 = prompt = ('¿Cual fue tu calificacion en la nota1?')
+let nota2 = prompt = ('¿Cual fue tu calificacion en la nota2?')
+let nota3 = prompt = ('¿Cual fue tu calificacion en la nota3?')
+let calcularPromedio = (nota1 , nota2 , nota3) / 3 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
